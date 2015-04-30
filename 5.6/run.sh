26a22ca8407e523c42e835bf5f422083f91f45b9
Gullit Miranda <gullitmiranda@gmail.com>
2015-04-30 16:51:01 -0300

    Adding dynamic support projects with the `public` folder for Apache, which are commonly used for frameworks.

--- 5.6/run.sh
+++ 5.6/run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 source /etc/apache2/envvars
-bindfs -u www-data -g www-data /app-origin /app
+
+DOCUMENT_ROOT="/app"
+
+if [ ! -z "$APP_DIR" ]; then
+	rm -rf ${DOCUMENT_ROOT}/*
+
+	if [ -d "${APP_DIR}/public" ]; then
+		dir="${DOCUMENT_ROOT}"
+	else
+		dir="${DOCUMENT_ROOT}/public"
+	fi
+
+	mkdir -p $dir
+
+	bindfs -u www-data -g www-data $APP_DIR $dir
+fi
+
 exec apache2 -D FOREGROUND
